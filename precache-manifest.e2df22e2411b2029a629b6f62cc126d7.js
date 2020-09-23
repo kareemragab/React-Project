f1f77a6d2d4ae562dc5d749f537ead8a1287a671
Kareem Hassan <ahmed.fakhry@tmentors.com>
2020-09-23 18:01:57 +0200

    Updates

---
+++ precache-manifest.e2df22e2411b2029a629b6f62cc126d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff644a75fa52449317d996b75e9f598",
+    "url": "/React-Project/index.html"
+  },
+  {
+    "revision": "1a7d515352b378496608",
+    "url": "/React-Project/static/css/main.93ebe51a.chunk.css"
+  },
+  {
+    "revision": "426d19a671f391ef97a6",
+    "url": "/React-Project/static/js/2.15a6010a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Project/static/js/2.15a6010a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a7d515352b378496608",
+    "url": "/React-Project/static/js/main.6c9dd7c4.chunk.js"
+  },
+  {
+    "revision": "524447e59fbee87d97bc",
+    "url": "/React-Project/static/js/runtime-main.e3071e08.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-Project/static/media/logo.5d5d9eef.svg"
+  }
+]);
